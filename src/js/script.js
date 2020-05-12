8e08b4e0cf314b833b3a51c40e01ff3405abed84
Vinicius Martins <contato@vinimartdev.com>
2020-05-12 01:38:41 -0300

    Add funções add e delete to-do

--- src/js/script.js
+++ src/js/script.js
@@ -4,62 +4,113 @@ const toDoList = document.querySelector(".app-list");
 const addBtn = document.getElementById("idAddBtn");
 const input = document.getElementById("idAddInput");
 
+let list = [];
+let id = 0;
 
+const check = "fa-check-circle";
+const uncheck = "fa-circle";
+const lined = "lined";
 
-function addToDo(toDo) {
+function addToDo(toDo, id, done, trash) {
+    if (trash) {
+        return;
+    }
 
-  function genId() {
-    let idNum = 1;
-    return idNum++;
-  }
+    this.toDo = toDo;
+    this.id = id;
+    this.done = done ? check : uncheck;
+    this.lined = this.done ? "" : lined;
 
-  this.id = `checkbox${genId()}`
+    this.toDoComponent = `
+  <div class="to-do-container">
 
-  this.toDo = toDo;
+  <div class="font-2x">
+      <i id="${this.id}" class="far ${this.done}" job="complete"></i>
+      <span class="to-do-title ${this.lined}">${this.toDo}</span>
+  </div>
 
-  this.toDoComponent = `
-<div class="to-do-container">
-    <div class="font-2x">
-      <input type="checkbox" id="${this.id}">
-      <label>${this.toDo}</label>
-    </div>
-    <div>
-      <i class="fas fa-trash-alt"></i>
-    </div>
+  <div>
+      <i id="trash${this.id}" class="fas fa-trash-alt" job="delete"></i>
+  </div>
 </div>
 `;
 
-  const position = "beforeend";
+    const position = "beforeend";
 
-  return toDoList.insertAdjacentHTML(position, this.toDoComponent);
+    return toDoList.insertAdjacentHTML(position, this.toDoComponent);
 }
 
-addBtn.addEventListener('click', () => {
-
-    let toDo = input.value
+addBtn.addEventListener("click", () => {
+    let toDo = input.value;
 
     if (toDo) {
-        addToDo(toDo)
+        addToDo(toDo, id, false, false);
+
+        list.push({
+            name: toDo,
+            id: id,
+            done: false,
+            trash: false,
+        });
     } else {
-        alert('Preencha o campo Add um To-do')
+        alert("Preencha o campo Add um To-do");
     }
 
-    return input.value = ''
+    return (input.value = "");
+});
 
-})
-
-input.addEventListener('keyup', (event) => {
-
-    let toDo = input.value
+input.addEventListener("keyup", (event) => {
+    let toDo = input.value;
 
     if (event.keyCode == 13) {
-
         if (toDo) {
-            addToDo(toDo)
+            addToDo(toDo, id, false, false);
+
+            list.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false,
+            });
         } else {
-            alert('Preencha o campo Add um To-do')
+            alert("Preencha o campo Add um To-do");
         }
-    
-        return input.value = ''
+        input.value = "";
+        id++;
     }
-})
+});
+
+function completeToDo(element) {
+    this.element = element;
+    this.element.classList.toggle(check);
+    this.element.classList.toggle(uncheck);
+    this.element.parentNode.querySelector(".to-do-title").classList.toggle(lined);
+
+    if (this.element.classList.contains(check)) {
+        list[this.element.id].done = true;
+    } else {
+        list[this.element.id].done = false;
+    }
+}
+
+function removeToDo(element) {
+    this.element = element;
+    this.element.parentNode.parentNode.parentNode.removeChild(
+        this.element.parentNode.parentNode
+    );
+
+    this.id = this.element.id;
+    this.id = this.id.replace("trash", "");
+    list[this.id].trash = true;
+}
+
+toDoList.addEventListener("click", function (event) {
+    let element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if (elementJob === "complete") {
+        completeToDo(element);
+    } else {
+        removeToDo(element);
+    }
+});
