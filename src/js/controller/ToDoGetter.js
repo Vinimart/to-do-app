1c7b3650e941bc6b77fe40f2db854690633beb42
Vinicius Martins <contato@vinimartdev.com>
2020-05-16 20:38:51 -0300

    eslint module

--- src/js/controller/ToDoGetter.js
+++ src/js/controller/ToDoGetter.js
@@ -13,7 +13,7 @@ class ToDoGetter {
         this.clear = toDo.clear;
         this.undo = toDo.undo;
 
-        this.data = localStorage.getItem("toDo");
+        this.data = localStorage.getItem('toDo');
 
         this.observe();
         this.storage();
@@ -23,7 +23,12 @@ class ToDoGetter {
     add() {
         // Passa os parâmetros padrões para o construtor do componente de to-do
         if (this.name.value) {
-            this.addToDo.insertComponent(this.name.value, this.id, false, false);
+            this.addToDo.insertComponent(
+                this.name.value,
+                this.id,
+                false,
+                false
+            );
 
             // Envia os parâmetros para a lista de objetos que armazenam as propriedades do componente to-do
             this.list.push({
@@ -33,14 +38,14 @@ class ToDoGetter {
                 trash: false,
             });
         } else {
-            alert("Preencha o campo Add um To-do");
+            alert('Preencha o campo Add um To-do');
             return;
         }
 
-        this.name.value = "";
+        this.name.value = '';
         this.id++;
 
-        localStorage.setItem("toDo", JSON.stringify(this.list));
+        localStorage.setItem('toDo', JSON.stringify(this.list));
     }
 
     completeToDo() {
@@ -48,7 +53,7 @@ class ToDoGetter {
         this.element.classList.toggle(this.addToDo.check);
         this.element.classList.toggle(this.addToDo.uncheck);
         this.element.parentNode
-            .querySelector(".to-do-title")
+            .querySelector('.to-do-title')
             .classList.toggle(this.addToDo.lined);
 
         // Altera a propriedade done no array lista do componente
@@ -58,28 +63,28 @@ class ToDoGetter {
             this.list[this.element.id].done = false;
         }
 
-        localStorage.setItem("toDo", JSON.stringify(this.list));
+        localStorage.setItem('toDo', JSON.stringify(this.list));
     }
 
     removeToDo() {
         // Altera a propriedade trash no array lista do componente
         this.trashId = this.element.id;
-        this.trashId = this.trashId.replace("trash", "");
+        this.trashId = this.trashId.replace('trash', '');
         this.list[this.trashId].trash = true;
 
         this.container = this.element.parentNode.parentNode;
 
         // Altera as classes CSS da instância do componente to-do
-        this.element.classList.add("color-5th");
-        this.container.classList.add("fade-out-left");
-        this.undo.classList.remove("hide");
+        this.element.classList.add('color-5th');
+        this.container.classList.add('fade-out-left');
+        this.undo.classList.remove('hide');
 
         // Remove o componente to-do logo após o fim da animação 'fade-out-left'
         window.setTimeout(() => {
             this.container.parentNode.removeChild(this.container);
         }, 400);
 
-        localStorage.setItem("toDo", JSON.stringify(this.list));
+        localStorage.setItem('toDo', JSON.stringify(this.list));
     }
 
     restoreToDo() {
@@ -93,45 +98,45 @@ class ToDoGetter {
 
         this.list[this.lastTrash.id].trash = false;
 
-        this.undo.classList.add("hide");
+        this.undo.classList.add('hide');
 
-        localStorage.setItem("toDo", JSON.stringify(this.list));
+        localStorage.setItem('toDo', JSON.stringify(this.list));
         location.reload();
     }
 
     observe() {
         // Add componente
-        this.name.addEventListener("keyup", (event) => {
+        this.name.addEventListener('keyup', (event) => {
             if (event.keyCode == 13) {
                 this.add();
             }
         });
 
         // Add componente
-        this.addBtn.addEventListener("click", () => {
+        this.addBtn.addEventListener('click', () => {
             this.add();
         });
 
         // Marca como concluído ou deleta o componente
-        this.toDoListView.addEventListener("click", (event) => {
+        this.toDoListView.addEventListener('click', (event) => {
             this.element = event.target;
             this.elementJob = this.element.attributes.job.value;
 
-            if (this.elementJob === "complete") {
+            if (this.elementJob === 'complete') {
                 this.completeToDo();
-            } else if (this.elementJob === "delete") {
+            } else if (this.elementJob === 'delete') {
                 this.removeToDo();
             }
         });
 
         // Limpa o localStorage e recarrega a página
-        this.clear.addEventListener("click", () => {
+        this.clear.addEventListener('click', () => {
             localStorage.clear();
             location.reload();
         });
 
         // Restaura o último to-do excluído
-        this.undo.addEventListener("click", () => {
+        this.undo.addEventListener('click', () => {
             this.restoreToDo();
         });
     }
@@ -151,7 +156,12 @@ class ToDoGetter {
     loadList(array) {
         // Passa para o construtor do componente as propriedades dos to-dos já existentes no localStorage
         array.forEach((item) => {
-            this.addToDo.insertComponent(item.name, item.id, item.done, item.trash);
+            this.addToDo.insertComponent(
+                item.name,
+                item.id,
+                item.done,
+                item.trash
+            );
         });
     }
 }
